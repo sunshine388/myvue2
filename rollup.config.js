4b73b696a5c0bc0b3469b2f982fe1f6f6f14e7d2
熄月 <2723166086@qq.com>
2022-08-09 17:48:34 +0800

    feat：搭建环境

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import babel from "rollup-plugin-babel";
+import serve from "rollup-plugin-serve";
+
+export default {
+  input: "./src/index.js", // 入口文件
+  output: {
+    format: "umd", // 支持amd 和 commonjs规范
+    name: "Vue",
+    file: "dist/vue.js", // 输出文件
+    sourcemap: true,
+  },
+  plugins: [
+    babel({
+      // 使用babel进行转化，但是排除node_modules文件
+      exclude: "node_modules/**", // glob 语法；**表示任意文件
+    }),
+    // dev环境下在3000端口开启一个服务
+    process.env.ENV === "development"
+      ? serve({
+          open: true,
+          openPage: "/public/index.html",
+          port: 3000,
+          contentBase: "",
+        })
+      : null,
+  ],
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+function Vue(options) {
+  // new Vue创建实例时会调用_init()方法
+  this._init(options);
+}
+export default Vue;
