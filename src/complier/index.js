8b292d68df42200e9c377f0145025ec7a9ec088f
熄月 <2723166086@qq.com>
2022-08-10 17:33:25 +0800

    feat：模板解析-生成ast语法树

---
+++ src/complier/index.js
@@ -0,0 +1,125 @@
+// 标签名 a-aaa
+const ncname = `[a-zA-Z_][\\-\\.0-9_a-zA-Z]*`;
+// 命名空间标签 aa:aa-xxx
+const qnameCapture = `((?:${ncname}\\:)?${ncname})`;
+// 开始标签
+const startTagOpen = new RegExp(`^<${qnameCapture}`); // 标签开头的正则 捕获的内容是标签名
+// 结束标签
+const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`); // 匹配标签结尾的 </div>
+// 匹配属性
+const attribute =
+  /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/;
+// 匹配标签结束的 >
+const startTagClose = /^\s*(\/?)>/;
+// 匹配 {{ }} 表达式
+const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g;
+
+// ast语法树，是用对象来描述原生语法的  虚拟dom，用对象描述dom节点
+export function complieToFuction(template) {
+  // 1，将模板变成 AST 语法树
+  let ast = parserHTML(template);
+  // 2，使用 AST 生成 render 函数
+  let code = generate(ast);
+}
+
+// 开始标签
+function start(tagName, attrs) {
+  console.log("开始标签", tagName, attrs);
+}
+// 结束标签
+function end(tagName) {
+  console.log("结束标签", tagName);
+}
+// 文本标签
+function text(chars) {
+  console.log("文本", chars);
+}
+
+// 将模板内容编译为ast语法树
+function parserHTML(html) {
+  /**
+   * 截取字符串
+   * @param {*} len 截取长度
+   */
+  function advance(len) {
+    html = html.substring(len);
+  }
+  /**
+   * 匹配开始标签，返回匹配结果
+   */
+  function parseStartTag() {
+    // 匹配开始标签，开始标签名为索引 1
+    const start = html.match(startTagOpen);
+    if (start) {
+      // 构造匹配结果，包含标签名和属性
+      const match = {
+        tagName: start[1],
+        attrs: [],
+      };
+      // 截取匹配到的结果
+      advance(start[0].length);
+
+      let end; // 是否匹配到开始标签的结束符号 > 或 />
+      let attr; // 存储属性匹配的结果
+      // 匹配属性且不能为开始的结束标签，例如：<div>，到 > 就已经结束了，不再继续匹配该标签内的属性
+      // 		attr = html.match(attribute)  匹配属性并赋值当前属性的匹配结果
+      // 		!(end = html.match(startTagClose))   没有匹配到开始标签的关闭符号 > 或 />
+      while (
+        !(end = html.match(startTagClose)) &&
+        (attr = html.match(attribute))
+      ) {
+        // 将匹配到的属性，push 到 attrs 数组中，匹配到关闭符号 >，while 就结束
+        match.attrs.push({
+          name: attr[1],
+          value: attr[3] || attr[4] || attr[5],
+        });
+        advance(attr[0].length); // 截取匹配到的属性 xxx=xxx
+      }
+      // 匹配到关闭符号 >,当前标签处理完成 while 结束
+      // 此时，<div id="app" 处理完成，需连同关闭符号 > 一起被截取掉
+      if (end) {
+        advance(end[0].length);
+      }
+      console.log("截取后的html:" + html);
+      // 开始标签处理完成后，返回匹配结果：tagName 标签名 + attrs属性
+      return match;
+    } else {
+      return false;
+    }
+  }
+
+  // 对模板不停截取，直至全部解析完毕
+  while (html) {
+    // 解析标签和文本
+    let index = html.indexOf("<");
+    if (index == 0) {
+      console.log("===============================");
+      console.log("解析 html：" + html + ",结果：是标签");
+      const startTagMatch = parseStartTag(); // 匹配开始标签，返回匹配结果
+      console.log("开始标签的匹配结果：", startTagMatch);
+      if (startTagMatch) {
+        // 匹配到开始标签，调用start方法，传递标签名和属性
+        start(startTagMatch.tagName, startTagMatch.attrs);
+        continue; // 如果是开始标签，无需执行下面的逻辑，继续下次 while 解析后续内容
+      }
+      // 没有匹配到开始标签，此时有可能为结束标签 </div>，继续处理结束标签
+      let endTagMatch = html.match(endTag);
+      if (endTagMatch) {
+        end(endTagMatch[1]);
+        advance(endTagMatch[0].length);
+        continue; // 如果是结束标签，无需执行下面逻辑，继续下次 while 解析后续内容
+      }
+    }
+    if (index > 0) {
+      console.log("解析 html：" + html + ",结果：是文本");
+      // 将文本取出来并发射出去,再从 html 中拿掉
+      let chars = html.substring(0, index); // hello</div>
+      text(chars);
+      advance(chars.length);
+    }
+  }
+}
+// 根据ast 语法树生成为 render 函数；
+function generate(ast) {
+  console.log("parserHTML-ast: " + ast);
+}
--- src/init.js
+++ src/init.js
@@ -1,4 +1,5 @@
 import { initState } from "./state";
+import { complieToFuction } from "./complier";
 
 export function initMixin(Vue) {
   Vue.prototype._init = function (options) {
@@ -9,10 +10,27 @@ export function initMixin(Vue) {
 
     // 初始化状态，包括initProps、initMethod、initData、initComputed、initWatch等
     initState(vm);
-    
+
     // 如果有el属性 进行模板渲染
     if (vm.$options.el) {
       console.log("有el,需要挂载");
+      vm.$mount(vm.$options.el);
+    }
+  };
+  Vue.prototype.$mount = function (el) {
+    const vm = this;
+    const opts = vm.$options;
+    el = document.querySelector(el);
+    vm.$el = el;
+    // 默认会查找有没有render，没有就template，没有template就采用el中的内容
+    if (!opts.render) {
+      let template = opts.template;
+      if (!template && el) {
+        template = el.outerHTML;
+      }
+      // 需要将template转换为render
+      const render = complieToFuction(template);
+      opts.render = render;
     }
   };
 }
