cbdef65c2a0f268c1962173b15dac7b494af298a
熄月 <2723166086@qq.com>
2022-08-10 15:02:11 +0800

    feat：数据代理的实现

--- src/state.js
+++ src/state.js
@@ -30,8 +30,30 @@ export function initState(vm) {
     // 对象劫持，用户改变数据，得到通知
     // Object.defineProperty，重写data上的所有属性，给属性增加get和set方法
     observe(data); // 使用 observe 实现 data 数据的响应式
+
+    // 当 vm.message 在 vm 实例上取值时，将它代理到vm._data上去取
+    for (let key in data) {
+      Proxy(vm, key, "_data");
+    }
   }
 
   function initComputed() {}
   function initWatch() {}
+}
+/**
+ * 代理方法
+ *  当取 vm.key 时，将它代理到 vm._data上去取
+ * @param {*} vm        vm 实例
+ * @param {*} key       属性名
+ * @param {*} source    代理目标，这里是vm._data
+ */
+function Proxy(vm, key, source) {
+  Object.defineProperty(vm, key, {
+    get() {
+      return vm[source][key];
+    },
+    set(newValue) {
+      vm[source][key] = newValue;
+    },
+  });
 }
