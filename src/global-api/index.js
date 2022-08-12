4c62f8337f6bcd01c606961bbe7aab54e35d389e
熄月 <2723166086@qq.com>
2022-08-12 16:36:20 +0800

    feat：Vue 初始化流程中的 Vue.component 实现

--- src/global-api/index.js
+++ src/global-api/index.js
@@ -1,4 +1,4 @@
-import { mergeOptions } from "../util";
+import { isObject, mergeOptions } from "../util";
 
 export function initGlobalAPI(Vue) {
   // 全局属性 options
@@ -9,7 +9,27 @@ export function initGlobalAPI(Vue) {
     console.log("打印mixins合并后的options、", this.options);
     return this; // 返回this,提供链式调用
   };
-  Vue.component = function (options) {};
+
+  /**
+   * 使用基础的 Vue 构造器，创造一个子类
+   * @param {*} definition
+   */
+  Vue.extend = function (definition) {};
+
+  /**
+   * Vue.component API
+   * @param {*} id          组件名
+   * @param {*} definition  组件定义
+   */
+  Vue.component = function (id, definition) {
+    definition.name = definition.name || id;
+    // 如果传入的definition是对象
+    if (isObject(definition)) {
+      definition = Vue.extend(definition);
+    }
+  };
+  // 将 definition 对象保存到全局：Vue.options.components
+  Vue.options.components[name] = definition;
   Vue.filte = function (options) {};
   Vue.directive = function (options) {};
 }
